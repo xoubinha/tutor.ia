447590ffc3937b72121ba9b00b8e671533a6eef5
Christian Carballo Lozano <24639237+ccarballolozano@users.noreply.github.com>
2024-10-08 23:46:42 +0200

    Feat/conversation (#10)
    Add conversation features:
    
    Improve initial question to query Azure Search
    Add chat history
    Update prompts
    Reshape output answer
    
    ---------
    
    Co-authored-by: xoubinha <matatahakuna555@gmail.com>

--- backend/app/api/endpoints/health.py
+++ backend/app/api/endpoints/health.py
@@ -4,6 +4,6 @@ from fastapi.responses import JSONResponse
 router = APIRouter()
 
 
-@router.get("/", response_class=JSONResponse)
+@router.get("", response_class=JSONResponse)
 def health_check():
     return {"status": "OK"}
